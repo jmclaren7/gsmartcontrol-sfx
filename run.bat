9132a55ec0fff22ff38bb093606ca6f5a68c59a3
John Mclaren <john@johnscs.com>
2024-04-16 15:57:26 -0400

    add safety check to run script

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 @echo off
 cd
 echo.
-echo Starting... Leave this window open, closing it could cause issues and will prevent cleaning temporary files.
+echo Starting... Leave this window open, closing it could prevent cleaning temporary files.
 
 cd gsmartcontrol
 cmd /c start "" gsmartcontrol.exe -q -l
@@ -14,16 +14,19 @@ if not errorlevel 1 (
     goto loop
 )
 
-
 echo.
 echo Program closed.
 echo.
-echo If this window doesn't close automaticly in about 5 seconds something has gone wrong and you may need
-echo   to force close it, the themporary files are located here: (%~dp0)
+echo If this window doesn't close automaticly you may need to force close it.
 echo.
+
+REM Exit if the script isn't in %temp%
+echo %~dp0 | findstr /C:"%temp%" >nul 2>&1
+if %errorlevel%==1 ( exit /b )
+
 echo Cleaning temporary files... 
 ping 127.0.0.1 -n 2 >nul
 rmdir /s /q "%~dp0"
 echo Done.
-ping 127.0.0.1 -n 1 >nul
+ping 127.0.0.1 -n 2 >nul
 exit
